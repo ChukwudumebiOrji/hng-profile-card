53718e3df7155bf15ad8d374e5cea801359c19f6
Morj <111486356+ChukwudumebiOrji@users.noreply.github.com>
2026-04-17 20:56:04 +0100

    Fix time update method to use textContent

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const timeElement = document.getElementById("current-time");
 
 const updateTime = () => {
-  timeElement.innerHTML = '<span>Date.now()<span>';
+  timeElement.textContent = Date.now();
 }
 
 updateTime();
